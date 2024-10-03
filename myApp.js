9687f4f87093934afb777f8e0de13836b8cddf87
Beatriz Cavallieri <bfcavallieri@gmail.com>
2024-10-03 19:32:09 -0300

    exercise: Create and Save a Record of a Model

--- myApp.js
+++ myApp.js
@@ -4,17 +4,23 @@ let mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String, required: true
+  },
   age: Number,
   favoriteFoods: [String]
-})
+});
 
 let Person = mongoose.model("Person", personSchema);
 
 mongoose.connect(process.env.MONGO_URI), { useNewUrlParser: true, useUnifiedTopology: true };
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  let person = new Person({ name: "Jane Doe", age: 30, favoriteFoods: ["kale", "lettuce"] });
+  person.save((error, data) => {
+    if (error) return console.error(error);
+    done(null, data);
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
